5b0560ab693a71b9232af07516d60e52bddeaf4d
lankina-2004-ui <lankina-2004@mail.ru>
2026-08-18 19:50:20 +0500

    Add files via upload

---
+++ src/Footer.tsx
@@ -0,0 +1,37 @@
+import footerDesktopVisual from '../Image9.png'
+import footerMobileVisual from '../footer-phone-mobile.png'
+import footerMobilePhone from '../footer-phone-mobile.png'
+
+function Footer() {
+  return (
+    <footer className="footer" aria-label="Контакты">
+      <img className="footer__visual footer__visual--desktop" src={footerDesktopVisual} alt="" aria-hidden="true" />
+      <img className="footer__visual footer__visual--mobile" src={footerMobileVisual} alt="" aria-hidden="true" />
+      <img className="footer__mobile-phone" src={footerMobilePhone} alt="" aria-hidden="true" />
+
+      <div className="footer__blog">
+        <p className="footer__blog-label">ЛИЧНЫЙ БЛОГ :)</p>
+        <a className="footer__blog-channel" href="https://t.me/anlankina" target="_blank" rel="noreferrer">
+          ТЕЛЕГРАМ КАНАЛ
+        </a>
+      </div>
+
+      <nav className="footer__socials" aria-label="Социальные сети">
+        <a className="footer__social footer__social--telegram" href="https://t.me/whygb" target="_blank" rel="noreferrer">
+          <span className="footer__social-desktop">Telegram</span>
+          <span className="footer__social-mobile">Телеграм</span>
+        </a>
+        <a className="footer__social footer__social--email" href="mailto:lankina-2004@mail.ru">
+          <span className="footer__social-desktop">Email</span>
+          <span className="footer__social-mobile">Почта</span>
+        </a>
+        <a className="footer__social footer__social--instagram" href="https://www.instagram.com/gloombabyy?igsh=MXdvb2ZyNjhlcHhsMQ==" target="_blank" rel="noreferrer">
+          <span className="footer__social-desktop">Instagram</span>
+          <span className="footer__social-mobile">Инстаграм</span>
+        </a>
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
